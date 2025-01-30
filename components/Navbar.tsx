7434c04d22eaaec46156f7cbdeb405a72c35101a
Coderx85 <work.priyanshu085@gmail.com>
2025-01-30 20:39:45 +0530

    Add AuthLayout component with feature cards and update Navbar image import

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-// import Image from 'next/image'
+import Image from 'next/image'
 import { SignedIn, UserButton } from '@clerk/nextjs'
 import MobileNav from './MobileNav'
 
@@ -20,13 +20,13 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
-        {/* <Image
+        <Image
           src="/icons/logo.svg"
           width={32}
           height={32}
           alt="yoom logo"
           className="max-sm:size-10"
-        /> */}
+        />
         <p className="text-[26px] font-extrabold group hover:text-primary text-white max-sm:hidden duration-75">
           Devn<span className='text-primary group-hover:text-white'>Talk</span>
         </p>
