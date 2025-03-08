ca260cac08088f79de5c5d82bafd79c4773b966c
Coderx85 <work.priyanshu085@gmail.com>
2025-03-08 14:15:21 +0530

    Update configuration and dependencies for improved development experience

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,8 +19,8 @@ const config = {
   	},
   	extend: {
   		colors: {
-			primary: '#1A11F5',
-			secondary: '#FF742E',
+  			primary: '#1A11F5',
+  			secondary: '#FF742E',
   			purple: '#5126B4',
   			blue: '#0E78F9',
   			yellow: '#F9A90E',
@@ -66,6 +66,16 @@ const config = {
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
   			}
   		},
   		keyframes: {
