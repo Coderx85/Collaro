3935be6620c1eff64fd54e349ab44f684d0bd283
Coderx85 <work.priyanshu085@gmail.com>
2025-03-13 00:01:43 +0530

    feat: add new icon components for Home, Personal, Previous, Reccording, and Upcoming

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,79 +2,16 @@
 
 import Link from 'next/link';
 import { useParams, usePathname } from 'next/navigation';
-import Image from 'next/image';
-
 import { cn } from '@/lib/utils';
 import { TooltipProvider } from '@radix-ui/react-tooltip';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
 import { sidebarLinks } from '@/constants';
 import { Button } from './ui/button';
-// import { useEffect } from 'react';
-
-
-// const sidebarLinks = [
-  //   {
-//     imgURL: '/icons/home.svg',
-//     route: '/dashboard',
-//     label: 'Home',
-//     details: 'This is the home page',
-//   },
-//   {
-//     imgURL: '/icons/upcoming.svg',
-//     route: '/dashboard/upcoming',
-//     label: 'Upcoming',
-//     details: 'List of upcoming meetings',
-//   },
-//   {
-//     imgURL: '/icons/previous.svg',
-//     route: '/dashboard/previous',
-//     label: 'Previous',
-//     details: 'List of previous meetings',
-//   },
-//   {
-//     imgURL: '/icons/Video.svg',
-//     route: '/dashboard/recordings',
-//     label: 'Recordings',
-//     details: 'List of recordings',
-//   },
-//   {
-//     imgURL: '/icons/add-personal.svg',
-//     route: '/dashboard/personal-room',
-//     label: 'Personal Room',
-//     details: 'User Personal detail ' 
-//   },
-// ];
-
 
 const Sidebar = () => {
   const pathname = usePathname();
   const params = useParams();
   const workspaceId = params?.workspaceId as string;
-  
-  // useEffect(() => {
-  //   const workspace = fetch('/api/workspace', {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     }})
-  //     .then((res) => {
-  //       if(!res.ok) {
-//         throw new Error('Workspace not found');
-  //       }
-  //       return res;
-  //     })
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //     });
-  //   if(workspace) {
-  //     console.log(workspace);
-  //   }
-
-  //   // setWorkSpace(workspace.id);
-  // }, []);
-
-
 
   return (
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 text-white max-sm:hidden lg:w-[264px]">
@@ -89,24 +26,19 @@ const Sidebar = () => {
           const isActive = pathname === route;
             return (
               <Tooltip delayDuration={1000} key={item.route}>
-                <TooltipTrigger key={index}> 
+                <TooltipTrigger key={index} className='group'> 
                   <Link
                     href={`/workspace/${workspaceId}${item.route}`}
                     key={item.label}
                     className={cn(
-                      'flex gap-4 items-center p-4 rounded-lg justify-start hover:bg-primary ease-in duration-100 hover:animate-out',
+                      'flex group gap-4 items-end p-4 rounded-lg justify-start hover:bg-white/20 hover:text-dark-2 ease-in duration-100 hover:animate-out',
                       {
-                        'bg-primary hover:animate-none': isActive,
+                        'bg-white/20 text-dark-2 hover:animate-none': isActive,
                       }
                     )}
                   >
-                  <p className="flex gap-3 text-lg font-semibold text-white max-lg:hidden">
-                    <Image 
-                      src={item.imgURL}
-                      alt={item.label}
-                      width={24}
-                      height={24}
-                    />
+                  <p className="flex gap-3 text-lg font-semibold text-current max-lg:hidden">
+                    <item.component selected={isActive}/>
                     {item.label}
                   </p>
                   </Link>
@@ -114,7 +46,7 @@ const Sidebar = () => {
                 <TooltipContent
                   className="z-10 bg-black/10 backdrop-blur-xl"
                   key={`${item.label}-content-${index}`}
-                  side='right'
+                  side='top'
                 >
                   <p className="text-sm text-white">{item.details}</p>
                 </TooltipContent>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,33 +1,39 @@
+import Home from "@/components/icons/Home";
+import Personal from "@/components/icons/Personal";
+import Previous from "@/components/icons/Previous";
+import Reccording from "@/components/icons/Reccording";
+import Upcoming from "@/components/icons/Upcoming";
+
 export const sidebarLinks = [
   {
-    imgURL: '/icons/home.svg',
     route: '',
     label: 'Home',
     details: 'This is the home page',
+    component: Home
   },
   {
-    imgURL: '/icons/upcoming.svg',
     route: '/upcoming',
     label: 'Upcoming',
     details: 'List of upcoming meetings',
+    component: Upcoming
   },
   {
-    imgURL: '/icons/previous.svg',
     route: '/previous',
     label: 'Previous',
     details: 'List of previous meetings',
+    component: Previous
   },
   {
-    imgURL: '/icons/Video.svg',
     route: '/recordings',
     label: 'Recordings',
     details: 'List of recordings',
+    component: Reccording
   },
   {
-    imgURL: '/icons/add-personal.svg',
     route: '/personal-room',
     label: 'Personal Room',
-    details: 'User Personal detail ' 
+    details: 'User Personal detail',
+    component: Personal 
   },
 ];
 
