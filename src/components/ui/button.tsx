e1c069b144669f777c7398fb2f087f54d9b5b141
Coderx85 <work.priyanshu085@gmail.com>
2025-03-25 00:12:21 +0530

    feat: improve type definitions and styling for button, input, carousel, popover, and tooltip components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,12 +5,12 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center cursor-pointer justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors duration-300 focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300',
+  'inline-flex items-center cursor-pointer justify-center whitespace-nowrap rounded-md font-medium ring-offset-white transition-colors duration-300 focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300',
   {
     variants: {
       variant: {
         default:
-          'text-bold bg-primary/50 text-white hover:bg-primary dark:hover:bg-primary',
+          'text-bold bg-primary text-white',
         destructive:
           'text-bold bg-red-500 text-2xl text-slate-50 hover:bg-red-500/90 dark:bg-red-700 dark:text-slate-50 dark:hover:bg-red-900/90',
         outline:
@@ -18,15 +18,15 @@ const buttonVariants = cva(
         secondary:
           'bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80',
         ghost:
-          'border-2 border-white hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50',
+          'border-2 border-white hover:bg-slate-100 text-5xl hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50',
         link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50',
         inactive: 'border-2 border-slate-600 bg-white text-slate-600',
         active: 'bg-alert text-white',
       },
       size: {
         default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-15 rounded-md px-4',
+        sm: 'h-9 w-9 rounded-md px-3',
+        lg: 'h-15 w-15 rounded-md px-4',
       },
     },
     defaultVariants: {
