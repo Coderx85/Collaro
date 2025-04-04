05c41600a2104c2ccd4077c2d4b98701078e4008
Coderx85 <work.priyanshu085@gmail.com>
2025-04-04 15:55:18 +0530

    feat: add Sidebarlink component for dynamic sidebar navigation; update Sidebar to use new component and fix role determination logic

--- src/app/(root)/workspace/[workspaceId]/@sidebar/page.tsx
+++ src/app/(root)/workspace/[workspaceId]/@sidebar/page.tsx
@@ -2,7 +2,7 @@ import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { currentUser } from "@clerk/nextjs/server";
 import LeaveTeamButton from "../_components/LeaveButton";
 import Image from "next/image";
-import Navlink from "./_components/items";
+import Navlink from "./_components/navlink";
 import AlertButton from "../_components/AlertButton";
 import { SidebarProps } from "@/types";
 
@@ -10,16 +10,17 @@ const Sidebar = async ({ params }: SidebarProps) => {
   const { workspaceId } = await params;
   const user = await currentUser();
 
-  const role = user?.publicMetadata?.role === "admin" ? "admin" : "member";
+  const role =
+    user?.publicMetadata?.role === "admin" || "owner" ? "admin" : "member";
 
   return (
     <section className="hidden xl:flex xl:flex-col xl:fixed left-0 top-0 h-full w-50 bg-gray-900 dark:bg-gray-600/50 text-white">
-      <div className="flex items-center gap-2 justify-self-start p-4.5">
+      <div className="flex items-center` gap-2 justify-self-start p-4.5">
         <Image
           src="/icons/logo.svg"
           width={32}
           height={32}
-          alt="yoom logo"
+          alt="devntalk logo"
           className="max-sm:size-10"
         />
         <p className="group text-[26px] font-extrabold text-white duration-75 hover:text-white max-sm:hidden">
