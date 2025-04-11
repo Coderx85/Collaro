944915bd606f35cb21737c1623f37e7e12bdad4f
Coderx85 <work.priyanshu085@gmail.com>
2025-04-11 20:25:17 +0530

    feat: enhance CardTitle styling for better contrast; implement arcjet middleware for rate limiting

--- src/components/MeetingCard.tsx
+++ src/components/MeetingCard.tsx
@@ -19,7 +19,7 @@ const MeetingCard = ({
   const { toast } = useToast();
 
   return (
-    <section className="flex min-h-[258px] w-full flex-col justify-between rounded-[14px] bg-zinc-950/90 dark:bg-dark-1 px-5 py-8 xl:max-w-[568px]">
+    <section className="flex min-h-[230px] w-full flex-col justify-between rounded-[14px] bg-zinc-950/90 dark:bg-dark-1 px-5 py-8 xl:max-w-[550px]">
       <article className="flex flex-col gap-5">
         <Image src={icon} alt="upcoming" width={28} height={28} />
         <div className="flex justify-between">
@@ -30,22 +30,6 @@ const MeetingCard = ({
         </div>
       </article>
       <article className={cn("flex justify-center relative", {})}>
-        {/* <div className='relative flex w-full max-sm:hidden'>
-          {avatarImages.map((img, index) => (
-            <Image
-              key={index}
-              src={img}
-              alt='attendees'
-              width={40}
-              height={40}
-              className={cn("rounded-full", { absolute: index > 0 })}
-              style={{ top: 0, left: index * 28 }}
-            />
-          ))}
-          <div className='flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4'>
-            +5
-          </div>
-        </div> */}
         {!isPreviousMeeting && (
           <div className="flex gap-2">
             <Button onClick={handleClick} className="rounded bg-blue px-6">
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-// import { auth } from '@clerk/nextjs/server';
+import arcjet, { shield } from "@arcjet/next";
+import { NextResponse } from "next/server";
 
 const protectedRoute = createRouteMatcher([
   "/workspace/:path*",
@@ -7,14 +8,31 @@ const protectedRoute = createRouteMatcher([
   "/meeting",
 ]);
 
+const API_KEY: string = process.env.NEXT_PUBLIC_ARCJET_KEY!;
+
+const aj = arcjet({
+  key: API_KEY,
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+  ],
+});
+
 const adminRoute = createRouteMatcher(["/admin/:path*", "meeting/:path*"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  // const { userId } = auth();
-  if (adminRoute(req)) {
-    // userId.role === 'admin'
+  const check = await aj.protect(req);
+
+  if (check.isDenied()) {
+    return NextResponse.json("Rate limit exceeded", { status: 403 });
   }
+
+  if (adminRoute(req)) await auth.protect();
+
   if (protectedRoute(req)) await auth.protect();
+
+  return NextResponse.next();
 });
 
 export const config = {
