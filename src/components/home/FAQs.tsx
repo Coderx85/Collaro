a52453add55d6f55691813cf523752f1b3236214
Coderx85 <work.priyanshu085@gmail.com>
2025-04-20 23:21:52 +0530

    feat: enhance FAQ and Feature components

--- src/components/home/FAQs.tsx
+++ src/components/home/FAQs.tsx
@@ -12,15 +12,17 @@ import { FaQuestionCircle } from "react-icons/fa";
 export default function FAQ() {
   return (
     <>
-      <h2 className="text-lg sm:text-xl flex justify-center items-center md:text-2xl font-bold text-center mb-4 sm:mb-6">
-        <FaQuestionCircle className="size-6 mr-3 dark:text-white text-black" />{" "}
+      <h2 className="text-lg sm:text-xl flex md:text-4xl font-bold text-center mb-4 sm:mb-6">
+        <FaQuestionCircle className="text-4xl mr-3 dark:text-yellow text-black" />{" "}
         Frequently Asked Questions
       </h2>
       <div className="w-full max-w-4xl mx-auto">
         <Accordion type="single" collapsible>
           {faqs.map((faq, index) => (
             <AccordionItem key={index} value={`faq-${index}`}>
-              <AccordionTrigger>{faq.question}</AccordionTrigger>
+              <AccordionTrigger>
+                {index + 1 + ". " + faq.question}
+              </AccordionTrigger>
               <AccordionContent>{faq.answer}</AccordionContent>
             </AccordionItem>
           ))}
