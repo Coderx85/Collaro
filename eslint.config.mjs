765890ae0750b7af465757fae1544ae65ab9b118
Coderx85 <work.priyanshu085@gmail.com>
2025-06-25 21:18:37 +0530

    chore: Update ESLint configuration and remove unused dependencies from package files.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import oxlint from "eslint-plugin-oxlint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,11 +13,11 @@ const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
+      "no-console": "warn",
       "no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
     },
   }),
-  ...oxlint.buildFromOxlintConfigFile("./oxlint.json"),
 ];
 
 export default eslintConfig;
