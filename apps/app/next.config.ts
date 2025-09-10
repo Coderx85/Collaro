a8c360b261b70d2add070a6c56f88858287b92db
Coderx85 <work.priyanshu085@gmail.com>
2025-09-11 00:08:57 +0530

    refactor: update configuration and dependencies for improved structure and consistency.

--- apps/app/next.config.ts
+++ apps/app/next.config.ts
@@ -1,16 +1,12 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone", // Optimize for Docker
-  outputFileTracingRoot: path.join(__dirname, "../../"), // Set the workspace root to fix the warning
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   images: {
